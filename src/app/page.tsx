e617a531d4522d870217d5e5927ba720850e6550
xh3b4sd <xh3b4sd@gmail.com>
2023-07-17 22:01:46 +0200

    rename label component to align with resource terminology (#2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import List from './list'
+import Label from './label'
 import Sort from './sort'
 
 export default function Page() {
   return (
     <main className="flex flex-col items-center pt-10 min-h-screen bg-gray-100">
       <div className="container relative mx-auto max-w-md h-10">
-        <List />
+        <Label />
         <Sort />
       </div>
     </main>
