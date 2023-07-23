7fe0d334064653beebf5316f41f2de81d7dd5cc0
xh3b4sd <xh3b4sd@gmail.com>
2023-07-23 21:25:10 +0200

    scale content via media queries and keep centered at all times (#4)

--- app/components/DarkMode.tsx
+++ app/components/DarkMode.tsx
@@ -23,7 +23,7 @@ export default function DarkMode() {
   return (
     <label className="relative inline-flex items-center cursor-pointer">
       <input type="checkbox" value="" checked={theme === "dark" ? true : false} onChange={toggleTheme} className="sr-only peer"></input>
-      <div className="w-8 h-4 bg-gray-200 peer-focus:outline-none peer-focus:ring-0 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-1 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+      <div className="w-8 h-4 bg-gray-300 peer-focus:outline-none peer-focus:ring-0 rounded-full peer after:content-[''] after:absolute after:top-1 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:after:translate-x-full peer-checked:after:border-blue-600 peer-checked:bg-blue-600"></div>
       <span className="ml-3 text-gray-900 dark:text-white">Dark Mode</span>
     </label>
   );
