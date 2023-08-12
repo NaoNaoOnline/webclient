ebe2a8cc2a4242d349963eb77230b0924c3aedbd
xh3b4sd <xh3b4sd@gmail.com>
2023-08-12 15:18:08 +0200

    add toasts for event create flow (#13)

---
+++ components/app/add-event/ErrorToast.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import * as Toast from '@radix-ui/react-toast';
+
+import { XMarkIcon } from '@heroicons/react/24/outline'
+
+interface Props {
+  error: Error;
+}
+
+export default function ErrorToast(props: Props) {
+  return (
+    <>
+      <Toast.Root
+        className="bg-white rounded-md shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] p-4 grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
+        duration={10000} // 10 seconds
+        style={{ backgroundColor: "#FF3D3D" }}
+      >
+        <Toast.Title className="[grid-area:_title] dark:text-black">
+          Error
+        </Toast.Title>
+        <Toast.Description className="[grid-area:_description] dark:text-black">
+          {props.error.message}
+        </Toast.Description>
+        <Toast.Close className="[grid-area:_action] text-gray-500 hover:text-gray-900" aria-label="Close">
+          <span aria-hidden>
+            <XMarkIcon className="m-2 w-6 h-6 dark:text-black" />
+          </span>
+        </Toast.Close>
+      </Toast.Root>
+      <Toast.Viewport className="[--viewport-padding:_25px] fixed top-0 right-0 flex flex-col p-[var(--viewport-padding)] gap-[10px] w-[390px] max-w-[100vw] m-0 list-none z-[2147483647] outline-none" />
+    </>
+  );
+};
--- components/app/add-event/SubmitToast.tsx
+++ components/app/add-event/SubmitToast.tsx
@@ -3,23 +3,49 @@ import * as Toast from '@radix-ui/react-toast';
 
 import { XMarkIcon } from '@heroicons/react/24/outline'
 
-export default function LoginToast() {
+interface Props {
+  callback: () => void;
+  completed: number;
+  failed: Error | null;
+}
+
+export default function SubmitToast(props: Props) {
+  const [duration, setDuration] = React.useState(31536000000); // 1 year
+
+  const onOpenChange = (open: boolean) => {
+    if (!open) {
+      props.callback()
+    }
+  };
+
+  React.useEffect(() => {
+    if (props.completed >= 100 || props.failed) {
+      setDuration(3000); // 3 seconds
+    }
+  }, [props.completed]);
+
   return (
-    <Toast.Provider swipeDirection="right">
+    <>
       <Toast.Root
         className="bg-white rounded-md shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] p-4 grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
-        duration={31536000000} // 1 year
+        duration={duration}
+        onOpenChange={onOpenChange}
       >
         <Toast.Title className="[grid-area:_title] dark:text-black">
-          You need to be logged in if you want to add a new event.
+          Adding New Event
         </Toast.Title>
-        <Toast.Close aria-label="Close">
+        <Toast.Description className="[grid-area:_description]">
+          <div className="mt-4 w-full rounded-full h-2 bg-gray-200">
+            <div style={{ width: `${props.completed}%` }} className={`bg-blue-600 h-2 rounded-full transition-width duration-1000 ease-in-out`}></div>
+          </div>
+        </Toast.Description>
+        <Toast.Close className="[grid-area:_action] text-gray-500 hover:text-gray-900" aria-label="Close">
           <span aria-hidden>
-            <XMarkIcon className="w-6 h-6 dark:text-black" />
+            <XMarkIcon className="m-2 w-6 h-6 dark:text-black" />
           </span>
         </Toast.Close>
       </Toast.Root>
       <Toast.Viewport className="[--viewport-padding:_25px] fixed top-0 right-0 flex flex-col p-[var(--viewport-padding)] gap-[10px] w-[390px] max-w-[100vw] m-0 list-none z-[2147483647] outline-none" />
-    </Toast.Provider>
+    </>
   );
 };
