5c8725566a6fbc595c188b360a5efa422f5bead7
xh3b4sd <xh3b4sd@gmail.com>
2023-08-29 15:52:42 +0200

    persist dark mode in local storage

--- components/app/sidebar/DarkMode.tsx
+++ components/app/sidebar/DarkMode.tsx
@@ -3,33 +3,46 @@ import React, { useState, useEffect } from "react";
 import { MoonIcon } from '@heroicons/react/24/outline'
 import { SunIcon } from '@heroicons/react/24/outline'
 
+const key: string = "config.naonao.online/theme"
+
 export default function DarkMode() {
-  const [theme, setTheme] = useState("light");
+  const [them, setThem] = useState<string>("light");
 
   const toggleTheme = () => {
-    if (theme === "light") {
-      setTheme("dark");
+    if (them === "light") {
+      setThem("dark");
+      localStorage.setItem(key, "dark");
     } else {
-      setTheme("light");
+      setThem("light");
+      localStorage.setItem(key, "light");
     }
   };
 
+  // Load the user's theme configuration from local storage when the component
+  // mounts client-side.
+  useEffect(() => {
+    const loc = localStorage.getItem(key);
+    if (loc) {
+      setThem(loc);
+    }
+  }, []);
+
   useEffect(
     () => {
-      document.body.className = theme;
+      document.body.className = them;
     },
-    [theme]
+    [them]
   );
 
   return (
     <div onClick={toggleTheme} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-gray-50 hover:bg-gray-200 dark:hover:bg-gray-700 group cursor-pointer">
-      {theme == "dark" && (
+      {them == "dark" && (
         <>
           <SunIcon className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
           <span className="flex-1 ml-3 whitespace-nowrap dark:group-hover:text-white">Light Mode</span>
         </>
       )}
-      {theme == "light" && (
+      {them == "light" && (
         <>
           <MoonIcon className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
           <span className="flex-1 ml-3 whitespace-nowrap dark:group-hover:text-white">Dark Mode</span>
