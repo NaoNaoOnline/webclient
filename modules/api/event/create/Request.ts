fdd2aa5beac6000139d5fef2f8e58fe85da013f9
xh3b4sd <xh3b4sd@gmail.com>
2023-09-09 21:57:39 +0200

    spare people typing the URL scheme (#38)

--- modules/api/event/create/Request.ts
+++ modules/api/event/create/Request.ts
@@ -1,3 +1,5 @@
+import spacetime from "spacetime";
+
 export interface EventCreateRequest {
   // local
   atkn: string;
@@ -15,46 +17,32 @@ export function NewEventCreateRequest(frm: FormData, atk: string, cat: string[],
   const sta = frm.get("start-input")?.toString() || "";
   const end = frm.get("end-input")?.toString() || "";
 
-  let d = newDura(sta, end)
-  let t = newTime(dat, sta)
-
   return {
     atkn: atk,
     cate: cat.join(','),
-    dura: d,
+    dura: newDura(sta, end),
     host: hos.join(','),
     link: newLink(lin),
-    time: t,
+    time: newTime(dat, sta),
   };
 }
 
-// newDura takes the start time and the end time of an event as string of
-// milliseconds according to the local Date time. The event duration is returned
-// as string of seconds.
+// newDura takes the start time and the end time of an event as spacetime "iso"
+// formatted strings according to the local Date time. The event duration is
+// returned as string of seconds.
 function newDura(sta: string, end: string): string {
-  const mil = new Date(parseInt(end, 10)).getTime() - new Date(parseInt(sta, 10)).getTime();
-  return (mil / 1000).toString();
+  return spacetime(sta).diff(spacetime(end), "second").toString();
 }
 
+// newLink adds the https:// prefix to the user input for the event link, since
+// we spare people typing the scheme every time, while the backend requires it.
 function newLink(lin: string): string {
-  return lin;
+  return "https://" + lin;
 }
 
-// newTime takes date and start time as string of milliseconds according to the
-// local Date time. The date parameter accounts for the day, month and year of
-// the event time. The start time parameter accounts for the hours and minutes
-// of the event taking place. Both parameters are merged and a string of unix
-// seconds of the merged Date instances is returned.
+// newTime takes the start date and the start time of an event as spacetime
+// "iso" formatted strings according to the local Date time. The event duration
+// is returned as string of unix seconds.
 function newTime(dat: string, sta: string): string {
-  const mrg = mrgDat(new Date(parseInt(dat, 10)), new Date(parseInt(sta, 10)));
-  mrg.setHours(mrg.getHours(), mrg.getMinutes(), 0, 0);
-  return (mrg.getTime() / 1000).toString();
-}
-
-function mrgDat(day: Date, hou: Date): Date {
-  const add = new Date(day);
-
-  add.setHours(hou.getHours(), hou.getMinutes(), 0, 0);
-
-  return add;
+  return (spacetime(sta).epoch / 1000).toString();
 }
