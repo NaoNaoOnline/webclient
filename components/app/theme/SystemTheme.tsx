ca3c478f28050340d15aec232bb295774f373e77
xh3b4sd <xh3b4sd@gmail.com>
2023-10-13 17:49:27 +0200

    allow webclient to sync theme with OS settings (#60)

---
+++ components/app/theme/SystemTheme.tsx
@@ -0,0 +1,98 @@
+import { createContext, useContext, useEffect } from "react";
+
+import { useManual } from "@/components/app/theme/ManualTheme";
+
+const systKey: string = "theme.naonao.online/system";
+
+export const SystemDefault = true;
+
+export const SystemContext = createContext<[
+  boolean,
+  (sys: boolean) => void
+]>([
+  getSystem(),
+  () => { },
+]);
+
+export function getSystem(): boolean {
+  if (typeof window === "undefined") {
+    return SystemDefault;
+  }
+
+  const loc = localStorage.getItem(systKey);
+  if (!loc) {
+    return SystemDefault;
+  }
+
+  if (loc.toLowerCase() === "true") {
+    return true;
+  }
+  if (loc.toLowerCase() === "false") {
+    return false;
+  }
+
+  return SystemDefault;
+}
+
+export function useSystem(): [boolean, (sys: boolean) => void] {
+  const [manu, setManu] = useManual();
+  const [syst, setSyst] = useContext(SystemContext);
+
+  // Provide an external setter for directly changing the internal state from
+  // the outside.
+  const updSyst = (sys: boolean) => {
+    setSyst(sys);
+    localStorage.setItem(systKey, String(sys));
+  };
+
+  useEffect(() => {
+    if (syst) {
+      const qry = window.matchMedia("(prefers-color-scheme: dark)");
+
+      // Sync with system theme when the user changes the app settings manually
+      // in the webclient settings.
+      if (qry.matches) {
+        setManu("dark");
+      } else {
+        setManu("light");
+      }
+
+      // Sync with system theme when the user changes the systems settings
+      // manually in the OS settings.
+      const onChange = (e: MediaQueryListEvent) => {
+        if (e.matches) {
+          setManu("dark");
+        } else {
+          setManu("light");
+        }
+      };
+
+      // Sync with system theme when the user visits the open browser tab again.
+      const onVisibilityChange = () => {
+        if (document.visibilityState === "visible") {
+          if (qry.matches) {
+            setManu("dark");
+          } else {
+            setManu("light");
+          }
+        }
+      };
+
+      qry.addEventListener("change", onChange);
+      document.addEventListener("visibilitychange", onVisibilityChange);
+
+      return () => {
+        qry.removeEventListener("change", onChange);
+        document.removeEventListener("visibilitychange", onVisibilityChange);
+      };
+    }
+  }, [syst]);
+
+  // Load the user's theme configuration from local storage when the component
+  // mounts client-side.
+  useEffect(() => {
+    setSyst(getSystem());
+  }, [setSyst]);
+
+  return [syst, updSyst];
+}
